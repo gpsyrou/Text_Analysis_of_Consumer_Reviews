427310536e6cbc62a1965a67a2a6164e1e371df4
George Spyrou <gspyrou@umich.edu>
2021-03-21 10:27:26 +0000

    create getter function for url to html object

--- reviews.py
+++ reviews.py
@@ -3,22 +3,37 @@ import numpy as np
 import requests
 
 from bs4 import BeautifulSoup
-import time
-import json
 
-import lxml.html as html
+import urllib
+
+target_url = 'https://uk.trustpilot.com/review/www.deliveroo.co.uk/fufu'
 
 
-target_url = 'https://uk.trustpilot.com/review/www.deliveroo.co.uk'
-request = requests.get(url=target_url, verify=False)
-page_content = request.content
+reponse_html.find_all("div", {"class": "headline__review-count"})
 
-tree = html.fromstring(page_content)
 
-def getOverallRatingCount(tree):
-    ratingCount = tree.xpath('//span[@class="headline__review-count"]')
-    return int(ratingCount[0].text.replace(',',''))
-   
 
-t = tree.xpath('//span[@class="review-content__text"]')
+def getHTMLObject(target_url: str):
+    '''
+    Given a website link (URL), retrieve the corresponding website in an html
+    format.
+
+    Parameters
+    ----------
+    target_url : str
+        URL of the webpage that will be transformed to a HTML object.
+    '''
+    print('Attempting to retrieve HTML object for {0}'.format(target_url))
+    request = urllib.request.urlopen(target_url)
+    if request.getcode() != 200:
+        raise Exception('Can not communicate with the client')        
+    else:
+        response = request.read()
+        response_html = BeautifulSoup(response, 'html.parser')
+        return response_html
+        
+
+test_html = getHTMLObject(target_url)
+print(test_html.prettify())
+
 

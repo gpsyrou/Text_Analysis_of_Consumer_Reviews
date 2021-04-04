52c1fd4193f02958c804229ab210d0658bc0922f
gpsyrou <gspyrou@umich.edu>
2021-04-04 10:57:51 +0100

    fix commit sync

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -1,4 +1,15 @@
+from typing import List
 
 class NoDataRetrievedError(Exception):
     def __init__(self):
         self.msg = 'No data could be retrieved or field was empty'
+
+
+def processedPages(input_file: str) -> List['str']:
+    """
+    Returns a list of all the links that are already processed.
+    """
+    with open(input_file, 'r') as file: 
+        file_content = [line.split('\t')[0].strip() for line in file.readlines()]
+        file.close()
+    return file_content

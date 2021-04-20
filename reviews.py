0e99a6ef21c5748e8109237451dcc18023ae5186
gpsyrou <gspyrou@umich.edu>
2021-04-20 20:33:22 +0100

    start creating of text processing package

--- reviews.py
+++ reviews.py
@@ -30,3 +30,53 @@ base_df['Date'] = pd.to_datetime(base_df['Date'], format="%Y-%m-%d %H:%M", error
 base_df['Rating'] = base_df['Rating'].apply(lambda row: splitRatingsColumn(row)[0]).astype(int)
 base_df['Rating_Text'] = base_df['Rating'].apply(lambda row: ratings_dict[row])
 
+# Are there reviewers that have submitted to more than one reviews ?
+reviewers_multiple =  base_df['Reviewer_Id'].value_counts()
+f = base_df[base_df['Reviewer_Id']==reviewers_multiple.index[0]]
+
+
+
+
+import nltk
+from typing import List
+from nltk.corpus import stopwords
+
+stopwords_ls = stopwords.words('english')
+
+test = base_df['Review'].iloc[0]
+
+nltk.word_tokenize(test,language='english')
+
+
+def tokenize_and_clean(text, punct=True, numerics=True) -> List[str]:
+    """
+    Performs tokenizations and cleaning processes given a document/text.
+    The function will always tokenize the given text but the cleaning tasks
+    are optional.
+    
+    Parameters
+    ----------
+    text: 
+        A document, which can be a word or sentence of arbitrary length.
+
+    punct: 
+        Indicator of removing tokens that are punctuation marks.
+    numerics: 
+        Indicator of removing tokens that correspond to numbers.
+
+    Returns
+    --------
+         A tokenized version of 'text' with the necessary updates depending on
+         the cleanup steps performed.
+    """
+    tokenized = nltk.word_tokenize(text, language='english')
+    
+    
+    
+    return tokenized
+
+
+
+def remove_stopwords(text: List['str'], stopwords_ls: List['str']) -> List['str']:
+    return [token for token in text if token not in stopwords_ls]
+

8afd47481837977f9a744ffb843f2cb99f733187
gpsyrou <gspyrou@umich.edu>
2021-07-28 18:39:26 +0100

    create notebook with working negative lda model

--- LDAModellerClass.py
+++ LDAModellerClass.py
@@ -6,18 +6,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 
-
 class LDAModeller:
     """ Common class used to perform an end-to-end Latent Dirichlet Allocation
     analysis. The base object of the class is a Pandas Dataframe.
     """
 
-
     def __init__(self, input_df: pd.DataFrame, reviews_col: str):
         self.input_df = input_df
         self.reviews_col = reviews_col
-        
-        
+
+
     def setCountVectorizer(self, max_df: float, min_df: float,
                            max_features: int, ngram_range: tuple):
         self.vectorizer = CountVectorizer(max_df=max_df, min_df=min_df,
@@ -58,8 +56,8 @@ class LDAModeller:
 
     def assignNGramsAsColumn(self, ngram_colname: 'str') -> None:
         self.input_df[ngram_colname] = self.identifyNGramsFromCountVectMatrix()
-
     
+
     def retrieveTopNgrams(self, top_n: int, n_gram_type='Bigram') -> None:
         self.n_gram_counts = self.countVectorizerToDict()
         if top_n is not None:
@@ -75,19 +73,19 @@ class LDAModeller:
         self.number_of_topics = topic_num
         if fit_model:
             self.fitted_lda_model = self.lda_model.fit(self.cv_matrix)
-            
-    
+
+
     def getPerTopicWordWeights(self, sort=True):
-        word_weights_per_topic = []
+        self.word_weights_per_topic = []
         for i, topic in enumerate(self.fitted_lda_model.components_):
             weights = list(zip(self.cv_feature_names, topic))
             if sort:
                 weights = sorted(weights, key=lambda x: x[1], reverse=True)
-            word_weights_per_topic.append([i, weights])
-        return self.word_weights_per_topic
-    
-    
+            self.word_weights_per_topic.append([i, weights])
+
+
     def showTopWordsPerTopic(self, num_top_words: int):
         for i in range(0, len(self.fitted_lda_model.components_)):
-            weights = self.getPerTopicWordWeights()[i][1]
+            self.getPerTopicWordWeights()
+            weights = self.word_weights_per_topic[i][1]
             print('Topic {0} : {1} \n'.format(i+1, weights[0:num_top_words]))
